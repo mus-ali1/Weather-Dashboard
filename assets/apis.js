86dc5b40b3dcc0c3b902ad591d4f175bd3b347fd
Mustafa Ali <mustafamire@gmail.com>
2021-07-28 11:00:15 +0100

    Addition of bootstrap

--- assets/apis.js
+++ assets/apis.js
@@ -2,6 +2,6 @@ var apiKey = "64364aff8f1f06d28b058755f3637866";
 
 var queryUrl = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
-var city;
+var city = "";
 
 fetch(queryUrl);
