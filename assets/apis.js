6a8ce33fac2666cc1853640778f421a2fcf9471f
Mustafa Ali <mustafamire@gmail.com>
2021-07-28 13:43:36 +0100

    Added generateUrl function

--- assets/apis.js
+++ assets/apis.js
@@ -1,7 +1,19 @@
 var apiKey = "64364aff8f1f06d28b058755f3637866";
 
-var queryUrl = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+function generateUrl(city) {
+
+    return `api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+}
+
+function makeRequest(url) {
+    return fetch(endpoint);
+
+}
+
+
 
 var city = "";
 
 fetch(queryUrl);
+
+console.log("hello from api ")
