8d6c9b6252d7bc2b1a7bc8f88dd1683c32461876
Mustafa Ali <mustafamire@gmail.com>
2021-08-04 16:37:16 +0100

    Add: declartion of variables for required elements

--- assets/apis.js
+++ assets/apis.js
@@ -1,12 +1,64 @@
 var apiKey = "64364aff8f1f06d28b058755f3637866";
+var searchInput = document.getElementById("search-input");
+var searchButton = document.getElementById("search-button");
+var searchHistory = document.getElementById("search-history");
 
-function generateUrl(city) {
 
-    return `https.//api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
-}
 
-function makeRequest(url) {
-    return fetch(endpoint).then(function (response) {
-        return response.json();
-    });
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function generateUrl(city) {
+
+//   return `https.//api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+// }
+
+// function makeRequest(url) {
+//   return fetch(generateUrl()).then(function (response) {
+//     return response.json();
+
+//   });
+// }
+// console.log(makeRequest)
+
+// function getApi() {
+//   // fetch request gets a list of all the repos for the node.js organization
+//   var requestUrl = "https.//api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}"
+
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data)
